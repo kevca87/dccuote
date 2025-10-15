75cc5f4cb5c4902345c8042008ada2a309a77826
Kevin Cespedes Arancibia <kev.cespedes.arancibia@gmail.com>
2025-10-15 14:28:25 -0300

    Improve structure of DailyQuoteProps

--- web/src/views/DailyQuoteView.tsx
+++ web/src/views/DailyQuoteView.tsx
@@ -1,17 +1,29 @@
+export type Character = {
+  id: string;
+  name: string;
+}
+
+export type Tag = {
+  id: string;
+  name: string;
+}
+
 export type DailyQuoteProps = {
+  id: string;
   quote: string;
-  author: string;
-  tags: string[];
+  character: Character;
+  source: string;
+  tags: Tag[];
 };
 
-function DailyQuote({ quote , author, tags }: DailyQuoteProps) {
+function DailyQuote({ quote , character, source, tags }: DailyQuoteProps) {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+    <div className="flex min-h-svh flex-col items-center justify-center p-10">
+      <h1 className="scroll-m-20 text-center text-4xl font-bold tracking-tight text-balance">
         {quote}
       </h1>
       <p className="mt-2 text-center text-lg text-muted-foreground">
-        - {author}
+        {character.name}
       </p>
     </div>
   );
