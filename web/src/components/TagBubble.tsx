31d4661a8d30c3eb700deddb04978f19b456fb4e
Kevin Cespedes Arancibia <kev.cespedes.arancibia@gmail.com>
2025-10-28 18:17:50 -0300

    Fix Tag hover

--- web/src/components/TagBubble.tsx
+++ web/src/components/TagBubble.tsx
@@ -2,7 +2,7 @@ export default function TagBubble({id, name}: {id: string; name: string}) {
   return (
     <div
       key={id}
-      className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 overflow-ellipsis hover:bg-gray-300 cursor-pointer"
+      className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 overflow-ellipsis cursor-pointer"
     >
       {name}
     </div>
